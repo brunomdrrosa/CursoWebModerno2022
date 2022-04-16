66df6150a095357cd0f5f88d09935c5895d7594a
Bruno Machado <brunomdr46@gmail.com>
2022-04-15 23:39:50 -0300

    Exercício de valores inversos

--- Javascript/exercicios.js
+++ Javascript/exercicios.js
@@ -56,7 +56,7 @@ console.log(maiorOuIgual(0, 0))
 
 // Escreva uma função que receba um valor booleano ou numérico. Se o parâmetro fornecido for booleano, o
 // retorno da função deverá ser o inverso. Por exemplo, se a entrada for false, retornará true. Se o parâmetro for
-// numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será 1. Se o parâmetro
+// numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será -1. Se o parâmetro
 // de entrada não for de nenhum dos tipo acima, retorne "booleano ou número esperados, mas o parâmetro é do
 // tipo ...".
 // Exemplos:
@@ -64,3 +64,13 @@ console.log(maiorOuIgual(0, 0))
 // inverso("6") // retornará "booleano ou número esperados, mas o parâmetro é do tipo string"
 // inverso(-2000) // retornará 2000
 // inverso("programação") // retornará "booleano ou números esperado, mas o parâmetro é do tipo string"
+function inverso(valor) {
+  if (typeof valor === "boolean") {
+    return !valor;
+  } else if (typeof valor === "number") {
+    return -valor;
+  } else {
+    return "booleano ou número esperados, mas o parâmetro é do tipo " + typeof valor;
+  }
+}
+console.log(inverso(true));
