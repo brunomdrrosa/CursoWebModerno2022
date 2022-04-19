9a2fdf32b3da40d84f64aa473e17de237a20eca3
Bruno Machado <brunomdr46@gmail.com>
2022-04-19 19:56:37 -0300

    Exercícios sobre Webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
+  }
+}
